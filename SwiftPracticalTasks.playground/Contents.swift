95461c6ceeb7498b9b87bb22274017ad4253a7ed
Paul Grin <pashkagrin@gmail.com>
2024-02-18 20:40:06 +0100

    Initial Commit

---
+++ SwiftPracticalTasks.playground/Contents.swift
@@ -0,0 +1,8 @@
+// 1. Поиск уникальных символов в строке
+
+var uniqueString = "Hello, Paul."
+var nonUniqueString = "Hello, Paul. Nice to meet you here!"
+
+func areAllSymbolsUnique(str: String) -> Bool {
+    return str.spl
+}
